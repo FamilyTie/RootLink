06e2df8a754cd1128ab7e817496f960cf7f7b224
bryanpow <ramosbusiness544@gmail.com>
2024-05-06 13:55:17 -0400

    started-debug

--- server/controllers/authControllers.ts
+++ server/controllers/authControllers.ts
@@ -4,7 +4,7 @@ import User from '../db/models/User';
 // the matching user in the database. If the user is found and the password
 // is valid, it adds the userId to the cookie (allowing them to stay logged in)
 // and sends back the user object.
-exports.loginUser = async (req, res) => {
+export const loginUser = async (req, res) => {
   const { username, password } = req.body // the req.body value is provided by the client
 
   const user = await User.findByUsername(username);
@@ -19,14 +19,14 @@ exports.loginUser = async (req, res) => {
 
 // This controller sets `req.session` to null, destroying the cookie 
 // which is the thing that keeps them logged in.
-exports.logoutUser = (req, res) => {
+export const logoutUser = (req, res) => {
   req.session = null;
   res.sendStatus(204);
 };
 
 // This controller returns 401 if the client is NOT logged in (doesn't have a cookie)
 // or returns the user based on the userId stored on the client's cookie
-exports.showMe = async (req, res) => {
+export const showMe = async (req, res) => {
   if (!req.session.userId) return res.sendStatus(401);
 
   const user = await User.find(req.session.userId);
---
+++ server/db/migrations/20240506175336_initial_tables.ts
@@ -0,0 +1,11 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+    knex
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+}
+
