f0090096d48146afa2ef7f6a36e5ddb9109e9fe9
bryanpow <ramosbusiness544@gmail.com>
2024-05-06 12:17:43 -0400

    Refactor javascript to typescript

--- server/routers/userRouter.ts
+++ server/routers/userRouter.ts
@@ -5,8 +5,6 @@ import {checkAuthentication} from '../middleware/checkAuthentication'
 export const userRouter = express.Router();
 userRouter.post('/', createUser);
 
-// These actions require users to be logged in (authentication)
-// Express lets us pass a piece of middleware to run for a specific endpoint
 userRouter.get('/', checkAuthentication, listUsers);
 userRouter.get('/:id', checkAuthentication, showUser);
 userRouter.patch('/:id', checkAuthentication, updateUser);
