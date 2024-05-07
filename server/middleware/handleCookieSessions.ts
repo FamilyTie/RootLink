2b3ffc43b8031a7de5b06dce39b9725ac08e5658
bryanpow <ramosbusiness544@gmail.com>
2024-05-07 08:19:37 -0400

    bugs

--- server/middleware/handleCookieSessions.ts
+++ server/middleware/handleCookieSessions.ts
@@ -6,7 +6,7 @@ export const handleCookieSessions = cookieSession({
   // which means until we close the browser. We like this for now!
   // But in real life you'd set the cookie to expire,
   // and implement an auto re-auth flow, but that's too much at this point.
-
-  // maxAge: 1000 * 60 * 60 * 24  // 24 hours
+  keys: ['key1', 'key2'],
+  maxAge: 1000 * 60 * 60 * 24  // 24 hours
 });
 
