d1132e89e22cb8873e98f2f5bb63c8489db2702a
ahmad <ahmadhamza052005@gmail.com>
2024-05-07 12:52:38 -0400

    added profiles controller and route to get profile by id /api/profiles/:id , tested with my own profile then removed it

--- server/index.ts
+++ server/index.ts
@@ -1,32 +1,32 @@
-require('dotenv').config();
-import path = require('path');
-import express = require('express');
+require("dotenv").config()
+import path = require("path")
+import express = require("express")
 
-import {handleCookieSessions} from './middleware/handleCookieSessions';
-import {logRoutes} from './middleware/logRoutes'
+import { handleCookieSessions } from "./middleware/handleCookieSessions"
+import { logRoutes } from "./middleware/logRoutes"
 
-import authRouter from './routers/authRouter'
-import userRouter from './routers/userRouter';
-
-const app = express();
+import authRouter from "./routers/authRouter"
+import userRouter from "./routers/userRouter"
+import { getProfileById } from "./controllers/profilesController"
+const app = express()
 
 // // middleware
-app.use(handleCookieSessions); // adds a session property to each request representing the cookie
-app.use(logRoutes); // print information about each incoming request
-app.use(express.json()); // parse incoming request bodies as JSON
-app.use(express.static(path.join(__dirname, '../frontend/dist'))); // Serve static assets from the dist folder of the frontend
+app.use(handleCookieSessions) // adds a session property to each request representing the cookie
+app.use(logRoutes) // print information about each incoming request
+app.use(express.json()) // parse incoming request bodies as JSON
+app.use(express.static(path.join(__dirname, "../frontend/dist"))) // Serve static assets from the dist folder of the frontend
 
-app.use('/api', authRouter);
-app.use('/api/users', userRouter);
-
+app.use("/api", authRouter)
+app.use("/api/users", userRouter)
+app.get("/api/profiles/:id", getProfileById)
 // Requests meant for the API will be sent along to the router.
 // For all other requests, send back the index.html file in the dist folder.
-app.get('*', (req, res, next) => {
-  if (req.originalUrl.startsWith('/api')) return next();
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-});
+app.get("*", (req, res, next) => {
+  if (req.originalUrl.startsWith("/api")) return next()
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+})
 
-const port = process.env.PORT || 1090;
+const port = process.env.PORT || 1090
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
-});
+  console.log(`Server running at http://localhost:${port}/`)
+})
