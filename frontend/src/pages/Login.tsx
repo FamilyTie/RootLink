8d1abc4bcc85cc201de5026ae1ea0d148a482050
bryanpow <ramosbusiness544@gmail.com>
2024-05-09 12:07:51 -0400

    branch

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -21,18 +21,23 @@ export default function LoginPage() {
 
   if (currentUser) return <Navigate to="/" />;
 
+
   return <>
-    <h1>Login</h1>
-    <form onSubmit={handleSubmit} aria-labelledby="login-heading">
-      <h2 id='login-heading'>Log back in!</h2>
-      <label htmlFor="username">Username</label>
-      <input type="text" autoComplete="username" id="username" name="username" />
+    <form  className=' relative inset-0 mx-[66.42%]   w-[337px] h-[435px] rounded-[1px] ' onSubmit={handleSubmit} aria-labelledby="login-heading">
+      <h2 id='login-heading ' className="font-400 text-[36px] m-auto text-center pt-10">Welcome back!</h2>
+      
+      <label htmlFor="username" className="text-[18px] pt-[3rem]  ml-[50px] m-auto font-500">Username:</label>
+      <input type="text" className="border border-[#0A69AE] ml-[50px] rounded-sm w-[210px]" autoComplete="username" id="username" name="username" />
 
-      <label htmlFor="password">Password</label>
-      <input type="password" autoComplete="current-password" id="password" name="password" />
-
-      <button>Log in!</button>
+      <label htmlFor="password" className="text-[18px] pt-[8px]  ml-[50px] m-auto font-500">Password</label>
+      <input type="password" className="border border-[#0A69AE]  ml-[50px] rounded-sm w-[210px]"  autoComplete="current-password" id="password" name="password" />
+  
+      <button  type='submit' className="w-[102px] h-[33px]    bg-[#042B48]   text-white ">Log-In</button>
     </form>
     { !!errorText && <p>{errorText}</p> }
   </>;
 }
+
+
+/* Rectangle 4 */
+
