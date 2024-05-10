60f52dd85c752f8c7c8a5bd6d1a0040bf0d45f17
bryanpow <ramosbusiness544@gmail.com>
2024-05-09 22:46:09 -0400

    logon + start home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,6 +1,43 @@
 export default function HomePage() {
   return <>
-    <h1>Home</h1>
+    <nav className="flex px-[8%] pt-[1.5rem] justify-between">
+      <div className="flex gap-1 ">
+      <h3 className="text-[36px] font-semibold">RootLink</h3>
+      
+      <img  className='w-[30.15px] h-[36.33px] self-center' src='/tree2.png' alt='tree' />
+      </div>
+      
+      <div>
+        <ul className="flex text-[32px] gap-4">
+          <li className="grad-blue1 ">Home</li>
+          <li>Sign-In</li>
+          
+        </ul>
+      </div>
+
+    </nav>
     <p>Put something interesting here!!</p>
   </>;
 }
+
+
+// /* Home */
+
+// position: absolute;
+// width: 70px;
+// height: 43px;
+// left: 1161px;
+// top: 22px;
+
+// font-family: 'Darker Grotesque';
+// font-style: normal;
+// font-weight: 600;
+// font-size: 32px;
+// line-height: 43px;
+
+// background: radial-gradient(50% 50% at 50% 50%, #0A69AE 0%, #0A69AE 50%, #042B48 100%);
+// -webkit-background-clip: text;
+// -webkit-text-fill-color: transparent;
+// background-clip: text;
+// text-fill-color: transparent;
+
