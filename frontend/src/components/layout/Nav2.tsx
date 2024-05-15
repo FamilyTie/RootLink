410eab488ae07a62f6151c001eb4920e01eb3c13
bryanpow <ramosbusiness544@gmail.com>
2024-05-15 13:30:31 -0400

    feed progress

--- frontend/src/components/layout/Nav2.tsx
+++ frontend/src/components/layout/Nav2.tsx
@@ -27,7 +27,7 @@ function Nav2() {
           ></img>
           <input
             placeholder={searchPlaceHolder}
-            className="  pb-1 pl-12 text-[1.3rem] text-[#9DADB8] font-medium  m-auto ml-[5rem] w-[22rem] h-[2.2rem]  rounded  border-[2px] "
+            className="  pb-1 pl-12 text-[1.3rem] placeholder:text-gray-300 text-[#9DADB8] font-medium  m-auto ml-[5rem] w-[22rem] h-[2.2rem]  rounded  border-[2px] "
           ></input>
         </div>
       </div>
